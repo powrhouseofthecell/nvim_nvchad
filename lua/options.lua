9e7bdfe8f734d1f4385a7875171b4a7e2e9d48fe
powrhouseofthecell <theshahzuhaib96@gmail.com>
2024-05-04 18:06:13 +0530

    chore: complete config for react, js, tsx, ts, prettier

---
+++ lua/options.lua
@@ -0,0 +1,8 @@
+require "nvchad.options"
+
+-- add yours here!
+
+local o = vim.o
+o.relativenumber = true
+-- o. = true
+-- o.cursorlineopt ='both' -- to enable cursorline!
---
+++ lua/plugins/init.lua
@@ -0,0 +1,71 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre', -- uncomment for format on save
+    config = function()
+      require "configs.conform"
+    end,
+  },
+
+  {
+    "tpope/vim-surround",
+  },
+
+  -- none-ls
+  {
+    "nvimtools/none-ls.nvim",
+    event = "VeryLazy",
+    opts = function()
+      return require "configs.null-ls"
+    end,
+  },
+
+  {
+    "windwp/nvim-ts-autotag",
+    ft = { "html", "javascript", "javascriptreact", "typescript", "typescriptreact" },
+    config = function()
+      require("nvim-ts-autotag").setup()
+    end,
+  },
+
+  -- These are some examples, uncomment them if you want to see them work!
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require("nvchad.configs.lspconfig").defaults()
+      require "configs.lspconfig"
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "lua-language-server",
+        "stylua",
+        "typescript-language-server",
+        "tailwindcss-language-server",
+        "html-lsp",
+        "css-lsp",
+        "prettier",
+        "eslint-lsp",
+      },
+    },
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "vim",
+        "lua",
+        "vimdoc",
+        "html",
+        "css",
+        "javascript",
+        "typescript",
+        "tsx",
+        "go",
+        "rust"
+      },
+    },
+  },
+}
