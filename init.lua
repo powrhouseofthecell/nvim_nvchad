82bb7633abaccc8ec1792cd7260eee17b3474c17
powrhouseofthecell <theshahzuhaib96@gmail.com>
2024-08-22 19:32:29 +0530

    chore: add clearRegister function and telescope for dotfile

--- init.lua
+++ init.lua
@@ -34,6 +34,80 @@ dofile(vim.g.base46_cache .. "statusline")
 
 require "nvchad.autocmds"
 
+
+require('telescope').setup({
+  defaults = {
+    -- configure to use ripgrep
+    vimgrep_arguments = {
+      "rg",
+      "--follow",        -- Follow symbolic links
+      "--hidden",        -- Search for hidden files
+      "--no-heading",    -- Don't group matches by each file
+      "--with-filename", -- Print the file path with the matched lines
+      "--line-number",   -- Show line numbers
+      "--column",        -- Show column numbers
+      "--smart-case",    -- Smart case search
+
+      -- Exclude some patterns from search
+      "--glob=!**/.git/*",
+      "--glob=!**/.idea/*",
+      "--glob=!**/.vscode/*",
+      "--glob=!**/build/*",
+      "--glob=!**/dist/*",
+      "--glob=!**/yarn.lock",
+      "--glob=!**/package-lock.json",
+    },
+
+  },
+  pickers = {
+    find_files = {
+      hidden = true,
+      -- needed to exclude some files & dirs from general search
+      -- when not included or specified in .gitignore
+      find_command = {
+        "rg",
+        "--files",
+        "--hidden",
+        "--glob=!**/.git/*",
+        "--glob=!**/.idea/*",
+        "--glob=!**/.vscode/*",
+        "--glob=!**/build/*",
+        "--glob=!**/dist/*",
+        "--glob=!**/yarn.lock",
+        "--glob=!**/package-lock.json",
+      },
+    },
+  },
+})
+
+
+-- Function to clear all registers in Neovim
+local function clear_registers()
+    -- Iterate through the ASCII values of registers a-z, A-Z, and 0-9
+    for i = 97, 122 do  -- a-z
+        vim.fn.setreg(string.char(i), "")
+    end
+    for i = 65, 90 do  -- A-Z
+        vim.fn.setreg(string.char(i), "")
+    end
+    for i = 48, 57 do  -- 0-9
+        vim.fn.setreg(string.char(i), "")
+    end
+    -- Clear special registers
+    vim.fn.setreg('-', "")
+    vim.fn.setreg('/', "")
+    vim.fn.setreg('"', "")
+    vim.fn.setreg('*', "")
+    vim.fn.setreg('+', "")
+end
+
+-- Create a user command to clear registers
+vim.api.nvim_create_user_command(
+    'ClearRegisters',
+    clear_registers,
+    { desc = 'Clear all registers' }
+)
+
 vim.schedule(function()
   require "mappings"
 end)
