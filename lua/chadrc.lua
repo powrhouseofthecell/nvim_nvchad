31e310c7c092132461c6a1fbb500511514d43ee5
powrhouseofthecell <theshahzuhaib96@gmail.com>
2024-08-22 19:34:49 +0530

    ui

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,19 +1,24 @@
--- This file  needs to have same structure as nvconfig.lua 
+-- This file  needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/NvChad/blob/v2.5/lua/nvconfig.lua
 
 ---@type ChadrcConfig
 local M = {}
 
 M.ui = {
-	theme = "monochrome",
-	-- theme = "chocolate",
-	-- theme = "material-darker",
-	-- theme = "poimandres",
-	-- theme = "pastelbeans",
-	hl_override = {
-		Comment = { italic = false },
-		["@comment"] = { italic = false },
-	},
+  -- theme = "dark_horizon",
+  -- theme = "mountain",
+  theme = "monochrome",
+  -- theme = "one_light",
+  -- theme = "radium",
+  -- theme = "oxocarbon",
+  -- theme = "chocolate",
+  -- theme = "material-darker",
+  -- theme = "poimandres",
+  -- theme = "pastelbeans",
+  hl_override = {
+    Comment = { italic = false },
+    ["@comment"] = { italic = false },
+  },
 }
 
 return M
