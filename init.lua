20ae3568ef62a62fd45eaff82d3cecabbe56b62f
powrhouseofthecell <theshahzuhaib96@gmail.com>
2024-09-15 15:09:35 +0530

    autocmd: highlight on yank (from kickstart)

--- init.lua
+++ init.lua
@@ -101,6 +101,17 @@ local function clear_registers()
     vim.fn.setreg('+', "")
 end
 
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
 -- Create a user command to clear registers
 vim.api.nvim_create_user_command(
     'ClearRegisters',
